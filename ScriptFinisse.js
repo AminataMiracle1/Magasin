0786207c78089429f2666216a92e7cae400ad146
info1 <2128896@cegepoutaouais.qc.ca>
2024-11-26 16:31:43 -0500

    Ajouter aet afficher fonctionne bien

--- ScriptFinisse.js
+++ ScriptFinisse.js
@@ -6,11 +6,10 @@
 function Magasin (){
     this.listesObjetMag = []
     this.afficheObjet = function (){
-
         // Récupere le tableau
         const $tableauBody = $("table tbody");
+        $tableauBody.empty(); // On vide le tableau phusique alors cela n'a pas n'impact sur la liste
         for(let objet of this.listesObjetMag){
-            console.log(objet.nom)
             // Ajouter une nouvelle ligne du tableau
             const $nouvelLigne = $("<tr></tr>")
             const $cel0 = $("<td></td>")
@@ -32,8 +31,6 @@ function Magasin (){
             // Ajouter la nouvelle ligne complètement au tableau
             $nouvelLigne.append($cel0, $cel1, $cel2, $cel3, $cel4);
             $tableauBody.append($nouvelLigne);
-
-            console.log("Nouvelle", $nouvelLigne);
         }
     }
 }
@@ -59,20 +56,37 @@ console.log(magasin.listesObjetMag)
 // Afficher les objet du magasin
 magasin.afficheObjet()
 
+
 function ajouterObjet (){
     // Récupere les valeurs du formulaire.
     let $nom = $("#nomObjet").val();
     let $pOffensive = $("#pOffensive").val();
     let $pDefensive = $("#pDefensive").val();
-    let $cout = $("#cout").val();
-    // Crééer  un objet
-    const objet  = new Objet($nom, $pOffensive, $pDefensive, $cout)
-    // Ajouter l'objet dans la listes des objets du Magasin
-    magasin.listesObjetMag.push(objet)
+    let $cout = parseFloat($("#cout").val());
 
-    console.log(magasin.listesObjetMag)
-    magasin.afficheObjet()
 
+    // Vérifier si les champs sont remplis
+    if (($nom.length) < 3 )
+        // TODO : Utiliser Bootstrap  Ou Css pour afficher les erreur
+        alert("Nom doit être superiere à 3 lettre");
+    else if ($pOffensive < 50 && $pOffensive > 100){
+        // TODO : Utiliser Bootstrap  Ou Css pour afficher les erreur
+        alert("La puissance offensive doit être plus de 50 et inferière à 100");
+    }else if ($pDefensive < 50 && $pDefensive > 100){
+        // TODO : Utiliser Bootstrap  Ou Css pour afficher les erreur
+        alert("La puissance Defensive doit être plus de 50 et inferière à 100");
+    } else if (isNaN($cout) || $cout <= 0) {
+        alert("Le prix doit être un nombre supérieur à 0$");
+    } else {
+        // Crééer  un objet
+        let objet  = new Objet($nom, $pOffensive, $pDefensive, $cout   )
+
+        // Ajouter l'objet dans la listes des objets du Magasin
+        magasin.listesObjetMag.push(objet)
+
+        console.log("La listes des objets ", magasin.listesObjetMag)
+        magasin.afficheObjet()
+    }
 }
 
 // gestion d'événement du btn Ajouter
