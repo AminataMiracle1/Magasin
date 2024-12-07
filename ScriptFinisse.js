4a70cbccdbf8336d22dd8c68fb89a00e6c24df2a
amina <2128896@cegepoutaouais.qc.ca>
2024-12-07 18:15:13 -0500

    La logique du code est fini maintenant rafiné

--- ScriptFinisse.js
+++ ScriptFinisse.js
@@ -8,12 +8,12 @@
  * @constructor
  */
 // Crée un objet avec ses propriétés
-function ObjetMag(id=0, nomObjet = "", puissanObjet = "", defenseObjet = "", cout = 0) {
+function ObjetMag(id = 0, nomObjet = "", puissanObjet = 0, defenseObjet = 0, cout = 0) {
     this.nom = nomObjet;
-    this.puissanObjet = puissanObjet;
-    this.defenseObjet = defenseObjet;
+    this.puissanObjet = parseFloat(puissanObjet);
+    this.defenseObjet = parseFloat(defenseObjet);
     this.cout = parseFloat(cout);
-    this.id = id
+    this.id = id;
 }
 function Magasin() {
     this.listesObjetMag = [];
@@ -34,22 +34,89 @@ function Magasin() {
             const $cel1 = $("<td></td>").text(objet.puissanObjet);
             const $cel2 = $("<td></td>").text(objet.defenseObjet);
             const $cel3 = $("<td></td>").text(`${objet.cout.toFixed(2)} $`);
-
             // Ajouter la cellule checkbox
             const $checkbox = $("<input>").attr("id", `${iD}`).attr("name", "achat").attr("type", "checkbox");
             const $cel4 = $("<td></td>").append($checkbox);
-
-
             // Ajouter la nouvelle ligne au tableau
             $nouvelleLigne.append($cel0, $cel1, $cel2, $cel3, $cel4);
             $tableauBody.append($nouvelleLigne);
         }
     };
+}
+/**
+ * Classe personnages
+ * @param nom
+ * @param attaque
+ * @param defense
+ * @param argent
+ * @param image
+ * @constructor
+ */
+let listPersonnage = []
+function Personnage(nom, attaque, defense, argent, image){
+    this.nomPer = nom;
+    this.attaquePer = parseInt(attaque);
+    this.defensePer = parseInt(defense) ;
+    this.argentPer = parseFloat(argent);
+    this.imagePer = image;
+    // une fonction qui affiche les personnages quand on choisi le personnage
+    this.affichePer = function (){
+        // les divs d affichage
+        let totalAttaque = $("#totalAttaque")
+        let totalDefensive = $("#totalDefensive")
+        let totalArgent = $("#totalArgent")
+        // Affecter les variables recut a leur place
+        totalAttaque.text(this.attaquePer);
+        totalDefensive.text(this.defensePer);
+        totalArgent.text((this.argentPer).toFixed(2) + " $")
+    }
+    this.attaqueAcheter = function (objetOffens) {
+        this.attaquePer = parseInt(this.attaquePer) + objetOffens;
+    }
+    this.defensiveAcheter = function (objetDeffensive) {
+        this.defensePer = parseInt(this.defensePer) + objetDeffensive;
+    }
+    this.argentAcheter = function (objetCout) {
+        this.argentPer = this.argentPer - objetCout;
+    }
+    // Afficher l'équipement acheter  : créer l'endroit
+    this.afficheObjet = function (ObjetNom) {
+        // Select l'endoit ou nous voulons afficher
+        let equipementAffiche  = $(".equipementAffiche")
+        // ajouter une liste dans le div
+        equipementAffiche.append("<ul>`${ObjetNom}`</ul>`");
+    }
+}
+/*************************************************************
+ *  Main de l'application
+ *  **********************************************************
+ */
+// Instancier un magasin
+let magasin = new Magasin();
 
+// Instancier des objets
+let objetMag1 = new ObjetMag(1, "GrosMarteau", 56, 60, 80);
+let objetMag2 = new ObjetMag(2, "Casque", 69, 80, 88);
+let objetMag3 = new ObjetMag(3, "Marteau", 97, 62, 98);
+let objetMag4 = new ObjetMag(4, "Épée", 87, 40, 28);
+
+//Instancier Personnages :
+let person1 = new Personnage("personJack", 12, 5, 350, "img/jack.png");
+let person2 = new Personnage("personLuffy", 45, 80, 3000, "img/luffy.png");
+person1.affichePer()
+
+listPersonnage.push(person1 , person2)
+// Ajouter des objets au magasin
+magasin.listesObjetMag.push(objetMag1, objetMag2, objetMag3, objetMag4);
+
+// Afficher les objets du magasin
+magasin.afficheObjet();
+
+// Gestion de l'événement pour le bouton Ajouter
+$("#btnAjouter").on("click", function () {
     /**
      * Ajouter un objet au magasin
      */
-    this.ajouterObjet = function () {
         // Récupère les valeurs du formulaire
         let $nom = $("#nomObjet").val();
         let $pOffensive = $("#pOffensive").val();
@@ -73,103 +140,22 @@ function Magasin() {
             alert("Le prix doit être supérieur à 0 $.");
             return;
         }
-        // Crée un nouvel objet
-        let objet = new ObjetMag($nom, $pOffensive, $pDefensive, $cout);
-
+        // Crée un nouvel objet et son ID
+        let ID = 1
+        for (element of magasin.listesObjetMag){
+            ID++
+            console.log("ID", ID)
+        }
+        let objet = new ObjetMag(ID,$nom, $pOffensive, $pDefensive, $cout);
         // Ajoute l'objet à la liste du magasin
-        this.listesObjetMag.push(objet);
-
-        console.log("Liste des objets :", this.listesObjetMag);
-
+        magasin.listesObjetMag.push(objet);
+        console.log("Liste des objets :", magasin.listesObjetMag);
         // Affiche les objets mis à jour
-        this.afficheObjet();
-    };
-}
-
-/**
- * Classe personnages
- * @param nom
- * @param attaque
- * @param defense
- * @param argent
- * @param image
- * @constructor
- */
-let listPersonnage = []
-function Personnage(nom, attaque, defense, argent, image){
-    this.nomPer = nom;
-    this.attaquePer = attaque;
-    this.defensePer = defense;
-    this.argentPer = parseFloat(argent);
-    this.imagePer = image;
-    // une fonction qui affiche les personnages quand on choisi le personnage
-    //
-    this.affichePer = function (){
-        // les divs d affichage
-        let totalAttaque = $("#totalAttaque")
-        let totalDefensive = $("#totalDefensive")
-        let totalArgent = $("#totalArgent")
-        // Affecter les variables recut a leur place
-        totalAttaque.text(this.attaquePer);
-        totalDefensive.text(this.defensePer);
-        totalArgent.text((this.argentPer).toFixed(2) + " $")
-    }
-
-    this.attaqueAcheter = function (objetOffens) {
-        this.attaquePer += objetOffens;
-    }
-
-    this.defensiveAcheter = function (objetDeffensive) {
-        this.defensePer += objetDeffensive;
-    }
-
-    this.argentAcheter = function (objetCout) {
-        this.argentPer -= objetCout;
-    }
-
-    // Afficher l'équipement acheter  : créer l'endroit
-    this.afficheObjet = function (ObjetAcheter) {
-        // Select l'endoit ou nous voulons afficher
-        let equipementAffiche  = $(".equipementAffiche")
-        // ajouter une liste dans le div
-        equipementAffiche.append(`<ul>\`${ObjetAcheter}\`</ul>`);
-    }
-
-
-}
-/*************************************************************
- *  Main de l'application
- *  **********************************************************
- */
-
-
-// Instancier un magasin
-let magasin = new Magasin();
-
-// Instancier des objets
-let objetMag1 = new ObjetMag( 1,"GrosMarteau", "67", "60", 78);
-let objetMag2 = new ObjetMag(2,"Casque", "70", "55", 67);
-
-//Instancier Personnages :
-let person1 = new Personnage("personJack", "12", "5", 350, "img/jack.png");
-let person2 = new Personnage("personLuffy", "45", "80", 3000, "img/luffy.png");
-person1.affichePer()
-
-listPersonnage.push(person1 , person2)
-// Ajouter des objets au magasin
-magasin.listesObjetMag.push(objetMag1, objetMag2);
-
-// Afficher les objets du magasin
-magasin.afficheObjet();
-
-// Gestion de l'événement pour le bouton Ajouter
-$("#btnAjouter").on("click", function () {
-    magasin.ajouterObjet();  // Appelle la méthode pour ajouter un objet
+        magasin.afficheObjet()
 });
 
 // Gestion de l'evenement de l'affiche des personnages.
 $("#lesPersonnages").on("change", function () {
-    let selectedValue = $("#lesPersonnages").val().trim().toLowerCase();
     for (let element of listPersonnage) {
         if (String(element.nomPer) === String($("#lesPersonnages").val())) {
             console.log("Condition satisfaite");
@@ -179,28 +165,68 @@ $("#lesPersonnages").on("change", function () {
     }
 });
 
-$("#btnAchat").on("click", function () {
+/**
+ * Une fonction qui récupère la personne qui achete
+ */
+function personAchete() {
     // On récupère la personne dans la boucle et le retourner
     let personActuelle = $("#lesPersonnages").val();
-    let personAchat;
-    let ObjetCout = []
-    for (let person of listPersonnage) {
-        if (personActuelle === person.nomPer) {
-            personAchat = person.nomPer;
-            console.log(personAchat);
-        }
-    }
-    // Récupérer les objet acheter
-    let objets = $("input[type='checkbox']");
-    for (let objetClik of objets) {
-        if (objetClik.checked) {
-            console.log("Condition satisfait", objetClik)
-            console.log("Condition satisfait", objetClik.id)
-            // Comparer l'id de l'objet clické au au ID des objet qui se trouve dans la liste d'objet
-            // utiliser un find une méthode Jquery
-        }
+    let personAchat = listPersonnage.find(person => person.nomPer === personActuelle)
+    return personAchat
+}
 
-    }
+/**
+ * Une fonctionne qui récupère les objets acheter
+ */
+function recupObjetAcheter() {
+    // Variable récupérer l'objet
+    let objetRecup = [];
+    // Récupérer les objets acheter.
+    let objetsClick = $("input[type='checkbox']:checked");
+    objetsClick.each(function (){
+        // Récupérer les id
+        let objetID = parseInt(this.id)
+        // On compare le ID au liste d'objet de la listes des magasin.
+        let objetAchat = magasin.listesObjetMag.find(objetMag =>objetMag.id === objetID);
+        if(objetAchat){
+            // Ajouter l'objet clicke dans la liste
+            objetRecup.push(objetAchat)
+        }
+    })
+    // returner la liste d'objet acheter
+    return objetRecup
+}
+$("#btnAchat").on("click", function () {
+    // Récuperer la personne
+    let personAchat = personAchete()
+    console.log(personAchat)
+    // Récuperer la listes d'objet acheter
+    let objetRecup = recupObjetAcheter()
+    /*
+    Calculer le total du caractère des l'objets achéter
+    // Calculer le somme de l'objet avec reduce somme = est la variable qui
+    accumule et obj et l'objet dans le tableau
+     */
+    let totalCout = objetRecup.reduce((somme, obj) => somme + obj.cout, 0);
+    let totalAttaque = objetRecup.reduce((somme, obj) => somme + parseFloat(obj.puissanObjet), 0);
+    let totalDeff = objetRecup.reduce((somme, obj) => somme + parseFloat(obj.defenseObjet), 0);
 
+    // Vérifier si la personne a assez d'argent pour acheter
+    if(totalCout > personAchat.argentPer){
+        alert("Vous n'avez pas assez d'argent")
+    }else {
+        // Appliquer les changements au personnage
+        personAchat.attaqueAcheter(parseInt(totalAttaque))
+        personAchat.defensiveAcheter(parseInt(totalDeff))
+        personAchat.argentAcheter(parseInt(totalCout))
+        personAchat.afficheObjet(objetRecup.nom) // erreur
+        // Afficher les nouvelles stats
+        personAchat.affichePer()
+        // Supprimer les objets achetés du magasin utilise filter et includes on supprimes tout les
+        // contenut dans listes d'objet récupérer
+        magasin.listesObjetMag = magasin.listesObjetMag.filter(objet => !objetRecup.includes(objet))
+        // afficher le tableau
+        magasin.afficheObjet()
+    }
 
 })
